88b7fb1b7986a0777d11505d43146c46bf207b25
Ayush Soni <141045200+AyushSoni14@users.noreply.github.com>
2025-07-03 10:27:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4932,7 +4932,9 @@ async def run_async_proxy_checks(url_country_pairs):
                     })
                     continue
 
-                proxy_url = f"http://{PROXY_CONFIG['username']}:{PROXY_CONFIG['password']}@{proxy_cfg['host']}:{proxy_cfg['port']}"
+                # proxy_url = f"http://{PROXY_CONFIG['username']}:{PROXY_CONFIG['password']}@{proxy_cfg['host']}:{proxy_cfg['port']}"
+                proxy_url = f"http://{PROXY_CONFIG['username']}:{PROXY_CONFIG['password']}@{country.lower()}.decodo.com:10000"
+                task = asyncio.create_task(
                 task = asyncio.create_task(
                     test_url_with_proxy_async(session, url, proxy_url)
                 )
